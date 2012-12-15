391f0a8691714cfbc085d9ffd4974e94f04dc673
Ilya Portnov <portnov@iportnov.ru>
2012-12-16 00:41:52 +0600

    Implement VM in Haskell. Why not? :)

--- tools/vm.hs
+++ tools/vm.hs
@@ -1,6 +1,11 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
+import Prelude hiding (catch)
+import Control.Concurrent
+import Control.Exception
 import Control.Monad
+import Data.IORef
+import Data.Char
 import Data.Word
 import Data.Int
 import Data.Bits
@@ -14,9 +19,12 @@ import System.IO
 import System.Environment
 import Text.Printf
 
+import Debug.Trace
+
 type Address = Int32
 
 type Memory = IOUArray Address Int32
+type Output = IORef [Int32]
 
 readBMP :: FilePath -> IO [Int32]
 readBMP path = do
@@ -28,26 +36,89 @@ readBMP path = do
       if end
         then return []
         else do
-             word <- get
+             word <- fromIntegral `fmap` getWord32le
              rest <- go
              return (word:rest)
 
-run :: Memory -> Address -> IO ()
-run memory ip = do
-  word <- readArray memory ip
-  let hiword :: Int16
-      hiword = fromIntegral $ word `shiftR` 16
-      loword :: Int16
-      loword = fromIntegral word .&. 0xffff
-  printf "IP= 0x%x: hi=0x%x lo=%d (word: %x)\n" ip hiword loword word
-  if loword == 0
-    then putStrLn "Lo = 0, stop."
-    else run memory $ ip + fromIntegral loword
+putc :: Output -> Int32 -> IO ()
+putc var x = do
+  val <- readIORef var
+  writeIORef var $ val ++ [x]
+
+run :: Integer -> Memory -> Output -> Address -> IO ()
+run j memory out ip
+  | j >= 10000 = fail $ "A cycle."
+  | otherwise = do
+
+    let readMem offset = {-trace ("R " ++ show offset) $-} readArray memory offset
+        writeMem offset value = writeArray memory offset value
+
+    word <- readMem ip
+
+    let hiword :: Int16
+        hiword = fromIntegral $ word `shiftR` 16
+        loword :: Int16
+        loword = fromIntegral word .&. 0xffff
+
+    let goNext = run (j+1) memory out $ ip + fromIntegral loword
+
+    -- printf "IP= 0x%x: hi=0x%x lo=%d (word: %x)\n" ip hiword loword word
+    if hiword <= 8
+       then do
+            let op :: Int32 -> Int32 -> Int32
+                op = case hiword of
+                       1 -> (+)
+                       2 -> (-)
+                       3 -> (*)
+                       4 -> div
+                       5 -> (.&.)
+                       6 -> (.|.)
+                       7 -> (\x y -> x `shiftL` fromIntegral y)
+                       8 -> (\x y -> x `shiftR` fromIntegral y)
+                       _ -> error $ "Unknown opcode:" ++ show hiword
+            offset <- readMem (ip + 2)
+            x <- readMem (ip + offset)
+            y <- readMem (ip + 3)
+            dstOffset <- readMem (ip + 1)
+            writeMem (ip + dstOffset) (x `op` y)
+            goNext
+       else case hiword of
+              9 -> do
+                   dstOffset <- readMem (ip + 1)
+                   x <- readMem (ip + 2)
+                   writeMem (ip + dstOffset) (complement x)
+                   goNext
+              10 -> do
+                    offset <- readMem (ip + 1)
+                    x <- readMem (ip + 2)
+                    y <- readMem (ip + 3)
+                    if x < y
+                      then run (j+1) memory out (ip + offset)
+                      else goNext
+              11 -> do
+                    c <- readMem (ip + 1)
+                    putc out c
+                    goNext
+              _  -> fail $ "Unknown opcode: " ++ show hiword
 
 main = do
-  [ips] <- getArgs
-  let ip = read ips
-  fileData <- readBMP "pic.bmp"
-  memory <- newListArray (0, 1337111-1) (fileData ++ repeat 0)
-  -- forM_ fileData print
-  run memory ip
+  [ips1, ips2] <- getArgs
+  let ip1 = read ips1
+      ip2 = read ips2
+  fileData <- readBMP "../pic.bmp"
+  forM_ [ip1 .. ip2] $ \ip -> do
+      memory <- newListArray (0, 1337111-1) (fileData ++ repeat 0)
+      forkIO $ do
+         out <- newIORef []
+         do
+           (do
+            run 0 memory out ip
+            res <- readIORef out
+            putStrLn $ "IP=" ++ show ip ++ ": " ++ map (chr . fromIntegral) res)
+          `catch`
+            (\(e :: SomeException) ->
+                do
+                res <- readIORef out
+                putStrLn $ "IP=" ++ show ip ++ ": " ++ map (chr . fromIntegral) res
+                print e)
+
