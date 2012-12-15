8742456c8b50f88299588f121d9eeb06f14973e4
ForNeVeR <neverthness@gmail.com>
2012-12-15 17:07:15 +0700

    VM implementation.

--- virtual-machine/main.cpp
+++ virtual-machine/main.cpp
@@ -1,13 +1,14 @@
-#include <iostream>
+#include <cstdint>
 #include <fstream>
+#include <iostream>
 
-void init_memory(int * const memory, const char * const path)
+void init_memory(int32_t * const memory, const char * const path)
 {
 	std::ifstream data(path);
-	int *ptr = memory;
+	int32_t *ptr = memory;
 
 	char buffer[4];
-	int count = 0;
+	int32_t count = 0;
 	while (count = data.readsome(buffer, 4))
 	{
 		std::memmove(ptr, buffer, count);
@@ -25,6 +26,58 @@ int main(int argc, char* argv[])
 
 	const char * const path = argv[1];
 
-	int * const memory = new int[13371111];
+	int32_t * const memory = new int32_t[13371111];
 	init_memory(memory, path);
+
+	size_t ip = 32;
+	while (true)
+	{
+		int32_t instruction = memory[ip];
+		int16_t command = instruction >> 16;
+		int16_t shift = instruction;
+		switch (command)
+		{
+		case 1:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] + memory[ip + 3];
+			break;
+		case 2:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] - memory[ip + 3];
+			break;
+		case 3:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] * memory[ip + 3];
+			break;
+		case 4:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] / memory[ip + 3];
+			break;
+		case 5:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] & memory[ip + 3];
+			break;
+		case 6:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] | memory[ip + 3];
+			break;
+		case 7:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] << memory[ip + 3];
+			break;
+		case 8:
+			memory[ip + memory[ip + 1]] = memory[ip + memory[ip + 2]] >> memory[ip + 3];
+			break;
+		case 9:
+			memory[ip + memory[ip + 1]] = ~memory[ip + 2];
+			break;
+		case 10:
+			if (memory[ip + 2] < memory[ip + 3])
+			{
+				ip += memory[ip + 1];
+			}
+			break;
+		case 11:
+			std::cout << (char)memory[ip + 1];
+			break;
+		default:
+			std::cerr << "Bad command " << command << " at " << ip << std::endl;
+			return 2;
+		}
+
+		ip += shift;
+	}
 }
