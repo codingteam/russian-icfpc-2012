2daa07bc69a4bbc537732b4959a1f6f21033ac91
ForNeVeR <neverthness@gmail.com>
2012-12-16 13:55:19 +0700

    something something

--- tools/hlvm.hs
+++ tools/hlvm.hs
@@ -1,3 +1,4 @@
+import Control.Concurrent.STM.TChan
 import System.Environment
 
 data Process = Process { id :: Integer
@@ -5,7 +6,18 @@ data Process = Process { id :: Integer
                        , variables :: [Char]
                        , m :: Integer
                        , v :: Char
-                       , s :: Integer }
+                       , s :: Integer
+                       , value :: Integer }
+
+send processId value = undefined
+
+step process chan = do
+  mapM (\id -> send id value) sendList
+  values <- mapM read [1..4] chan
+  process { value = getValue values }
+    where getValue vs = (m process) * result / 64 + (s process)
+          result   vs = ((sum vs) + 2) / 4
+
 
 main = do
   [filename] <- getArgs
